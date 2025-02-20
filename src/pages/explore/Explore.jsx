9ba8e39667d16911f252d0dace7702430681bd37
NUR RUKHAN SAPUTRA <108443287+Rukh4n@users.noreply.github.com>
2025-02-20 15:00:14 +0700

    add animation

---
+++ src/pages/explore/Explore.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const Explore = () => {
+  // useEffect(() => {
+  //   const fetchJobs = async () => {
+  //     try {
+  //       const response = await axios.get("https://remoteliveapi.com/api/jobs"); // Ganti dengan endpoint yang benar jika berbeda
+  //       console.log("Data Jobs:", response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching jobs:", error);
+  //     }
+  //   };
+
+  //   fetchJobs();
+  // }, []);
+
+  return <div>Explore</div>;
+};
+
+export default Explore;
